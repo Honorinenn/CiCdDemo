ec2cc739abc4d4a71c7176bd8d6d6891759fd829
Honorine Ndom <honorinendzah@gmail.com>
2022-06-02 08:00:37 -0700

    changes for test to pass

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -1,6 +1,20 @@
 package com.trilogyed.hellocircle.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloCircleController {
 
+
+    @GetMapping(value = "/hello")
+    public String helloCircle() {
+        return "Hello, Circle!";
+    }
+
+    @GetMapping(value = "/answer")
+    public String getMeaningOfLifeUniverseAndEverything() {
+        return "The answer is 42!";
+    }
 }
